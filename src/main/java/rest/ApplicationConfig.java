e2f74fec47e9a19d2ef0404799075e0fc32dfd24
Bearukun <bearukun@gmail.com>
2017-03-13 15:51:56 +0100

    Small change to REST

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package rest;
 
 import java.util.Set;
 import javax.ws.rs.core.Application;
 
-/**
- *
- * @author bearu
- */
-@javax.ws.rs.ApplicationPath("webresources")
+
+@javax.ws.rs.ApplicationPath("wapi")
 public class ApplicationConfig extends Application {
 
     @Override
