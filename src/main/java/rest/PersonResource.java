2ec9d7d0784eb5a41389ab4bf188e09de3145c5c
RadeonXRay <ceo_titanic@msn.com>
2017-03-16 22:29:28 +0100

    added exception for addPerson

--- src/main/java/rest/PersonResource.java
+++ src/main/java/rest/PersonResource.java
@@ -61,7 +61,7 @@ public class PersonResource {
 
         Person person = facade.getPerson(id);
         System.out.println("Hello! " + person);
-
+        System.out.println(facade.getPersons().size());
         if (person == null) {
 
             throw new PersonException("404");
@@ -84,12 +84,16 @@ public class PersonResource {
      * Method to return a specific person object from the mySQL database.
      *
      * @return A list with every object in JSON format.
+     * @throws exception.PersonException
      */
     @GET
     @Path("/contactinfo")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getPersonsContactinfo() {
-
+    public String getPersonsContactinfo() throws PersonException {
+        int pSize = facade.getPersons().size();
+        if( pSize < 0){
+        throw new PersonException("500");
+        }
         return gson.toJson(facade.getPersonsContactinfo());
 
     }
@@ -102,17 +106,56 @@ public class PersonResource {
     @GET
     @Path("/contactinfo/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getPersonContactinfo(@PathParam("id") int id) {
+    public String getPersonContactinfo(@PathParam("id") int id) throws PersonException{
 
-        return gson.toJson(facade.getPersonsContactinfo(id));
+        Person person = facade.getPerson(id);
+        System.out.println("Hello! " + person);
+        if (person == null) {
 
+            throw new PersonException("404");
+
+        }
+
+        try {
+
+            return gson.toJson(facade.getPersonsContactinfo(id));
+
+        } catch (NumberFormatException e) {
+
+            return "";
+        
+
+    }
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public String addPerson(String json) {
+    public String addPerson(String json) throws PersonException{
         
         Person person = gson.fromJson(json, Person.class);
+        
+        //check if the required name and email information has been added
+        if (person.getFirstName() == null || 
+                person.getLastName() == null ||
+                person.getEmail() == null) {
+            System.out.println("Person not added: Failed Step 1");
+            throw new PersonException("400" + "addStep1");
+        } 
+        //Check if the required adress informatino has been added
+        else if ( person.getAddress().getAdditionalInfo() == null || 
+                person.getAddress().getCityInfo() == null ||
+                person.getAddress().getStreet() == null){
+             System.out.println("Person not added: Failed Step 2");
+            throw new PersonException("400" + "addStep2");
+            
+        } 
+        //Check if any phones has been added
+        else if (person.getPhones().get(0) == null){
+             System.out.println("Person not added: Failed Phone Step");
+            throw new PersonException("400" + "addStepPhone");
+        } else
+        
+        System.out.println("Person succesfully added");
         facade.addPerson(person);
         return gson.toJson(person);
         
